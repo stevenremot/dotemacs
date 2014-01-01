b720ce038740780eee777fbff4dc2e1afefe6b29
Steven Rémot <steven.remot@gmail.com>
2014-01-01 13:34:38 +0100

    Externalized personnal theme in a theme file, and changed default theme

--- custom-configuration.el
+++ custom-configuration.el
@@ -3,11 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (wombat)))
+ '(custom-safe-themes (quote ("026a20335f54d3326cbbf6eb7934c203004bfa8d6a1da8156c9ff817b1c8580a" "05005fac4a123b966105fd2fd637b724e9d7030c5e14af4086c237b091d16396" "aaa5c988b99658cd96a17da864266870afe26033c71976cb5609a797c98dfe4c" default)))
  '(ecb-layout-window-sizes (quote (("left8" (ecb-directories-buffer-name 0.16666666666666666 . 0.2619047619047619) (ecb-sources-buffer-name 0.16666666666666666 . 0.23809523809523808) (ecb-methods-buffer-name 0.16666666666666666 . 0.2857142857142857) (ecb-history-buffer-name 0.16666666666666666 . 0.19047619047619047)))))
  '(ecb-options-version "2.40")
  '(ede-project-directories (quote ("/home/steven/Documents/Projets/skeleton-2d" "/home/steven/Documents/Programmation/C++/test")))
- '(electric-indent-mode t)
- '(electric-pair-mode t)
  '(flycheck-clang-include-path (quote ("../include")))
  '(flycheck-clang-language-standard "c++11")
  '(inferior-lisp-program "sbcl")
@@ -15,22 +16,4 @@
  '(rst-adornment-faces-alist (quote ((t . font-lock-keyword-face) (nil . font-lock-keyword-face) (1 . font-lock-keyword-face) (t . font-lock-keyword-face) (3 . font-lock-keyword-face) (4 . font-lock-keyword-face) (5 . font-lock-keyword-face) (6 . font-lock-keyword-face))))
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 587)
- '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "orange" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
- '(bold ((t (:weight bold))))
- '(cursor ((t (:background "grey"))))
- '(ecb-analyse-face ((t (:inherit ecb-default-highlight-face))))
- '(ecb-default-highlight-face ((t (:background "medium blue"))))
- '(ecb-directory-face ((t (:inherit ecb-default-highlight-face))))
- '(ecb-history-face ((t (:inherit ecb-default-highlight-face))))
- '(ecb-method-face ((t (:inherit ecb-default-highlight-face))))
- '(ecb-source-face ((t (:inherit ecb-default-highlight-face))))
- '(ecb-tag-header-face ((t (:background "dark green"))))
- '(error ((t (:foreground "red1" :weight thin))))
- '(region ((t (:background "indian red")))))
+ '(smtpmail-smtp-service 587))
