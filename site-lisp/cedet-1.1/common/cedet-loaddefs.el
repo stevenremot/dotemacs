438ca8ca99e87076b044307bd6ecea89bfb79b18
Steven Rémot <steven.remot@gmail.com>
2014-02-26 21:40:48 +0100

    Fix CEDET compilation

--- site-lisp/cedet-1.1/common/cedet-loaddefs.el
+++ site-lisp/cedet-1.1/common/cedet-loaddefs.el
@@ -594,7 +594,7 @@ Only pulses the line if `pulse-command-advice-flag' is non-nil.
 ;;;***
 
 ;;;### (autoloads nil nil ("cedet-load.el" "cedet.el" "ezimage.el"
-;;;;;;  "working.el") (21262 19590 216270 842000))
+;;;;;;  "working.el") (21262 20706 545826 994000))
 
 ;;;***
 
--- site-lisp/cedet-1.1/speedbar/speedbar-loaddefs.el
+++ site-lisp/cedet-1.1/speedbar/speedbar-loaddefs.el
@@ -98,7 +98,7 @@ selected.  If the speedbar frame is active, then select the attached frame.
 
 ;;;### (autoloads nil nil ("bigclock.el" "dframe.el" "sb-ant.el"
 ;;;;;;  "sb-html.el" "sb-image.el" "sb-texinfo.el" "speedbar-load.el")
-;;;;;;  (21262 19592 669484 681000))
+;;;;;;  (21262 20709 32221 26000))
 
 ;;;***
 
