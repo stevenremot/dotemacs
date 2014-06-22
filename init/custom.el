6882f70e04ccc52dbf47b0944dd868fffa2aafa7
Steven Rémot <steven.remot@gmail.com>
2014-06-22 10:34:25 +0200

    Add site-lisp in custom module

--- init/custom.el
+++ init/custom.el
@@ -6,9 +6,12 @@
 
 (defvar my-confdir)
 (defconst my-custom-conf-file (concat my-confdir "custom-configuration.el"))
+(defconst my-site-lisp (concat my-confdir "site-lisp/"))
 
 (setq custom-file my-custom-conf-file)
 (load-file my-custom-conf-file)
+
+(add-to-list 'load-path my-site-lisp)
 
 (provide 'custom)
 
