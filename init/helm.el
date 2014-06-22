455892df57917cfc00bcd4cd2d600303d452551d
Steven Rémot <steven.remot@gmail.com>
2014-06-22 11:00:35 +0200

    Fix helm init

--- init/helm.el
+++ init/helm.el
@@ -6,10 +6,11 @@
 (require 'use-package)
 
 (use-package helm
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x b" . helm-buffers-list)
-         ("C-x C-b" . helm-buffers-list)))
+  :init (progn
+          (global-set-key (kbd "M-x") 'helm-M-x)
+          (global-set-key (kbd "C-x C-f") 'helm-find-files)
+          (global-set-key (kbd "C-x b") 'helm-buffers-list)
+          (global-set-key (kbd "C-x C-b") 'helm-buffers-list)))
 
 (provide 'init/helm)
 
