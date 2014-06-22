9194b7de33cd22074d1a1e34d8dfbb9417205dae
Steven Rémot <steven.remot@gmail.com>
2014-06-22 11:03:20 +0200

    Add search macro in package.el

--- init/package.el
+++ init/package.el
@@ -6,6 +6,23 @@
 ;;; Code:
 (require 'package)
 
+(fset 'my-show-installed-packages
+      (lambda (&optional arg)
+        "Show installed packages."
+        (interactive "p")
+        (kmacro-exec-ring-item
+         (quote ([134217843 111 dead-circumflex 32 73 return] 0 "%d")) arg)))
+
+(eval-after-load 'package
+  '(define-key package-menu-mode-map
+     (kbd "C-c s")
+     'perso-show-installed-packages))
+
+(eval-after-load 'package
+  '(define-key package-menu-mode-map
+     (kbd "C-c s")
+     'perso-show-installed-packages))
+
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives
                '("gnu" .
