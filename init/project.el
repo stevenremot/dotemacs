ee4c6efc83769dd7d0030ebd0de3def8054a13c3
= <steven.remot@telecom-paristech.fr>
2014-10-01 19:04:32 +0200

    Avoid projectile-global-mode
    It doesn't play nicely with TRAMP

--- init/project.el
+++ init/project.el
@@ -5,9 +5,24 @@
 ;;; Code:
 (require 'use-package)
 
+(defun init-project-add-projectile-hooks ()
+  "Init projectile on supported modes.
+
+Do not launch projectile in global mode because it doesn't play
+nicely with TRAMP."
+  (dolist (mode '(fundamental-mode-hook
+                  prog-mode-hook
+                  js3-mode-hook
+                  conf-mode-hook
+                  dired-mode-hook
+                  org-mode-hook))
+    (add-hook mode 'projectile-mode)))
+
 (use-package projectile
   :ensure projectile
-  :init (projectile-global-mode 1))
+  :init (init-project-add-projectile-hooks)
+  ;; Conflicts with svn, so using native cahing method for now.
+  :config (progn (setq projectile-enable-caching t)))
 
 (use-package helm-projectile
   :ensure helm-projectile)
