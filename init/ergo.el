84d57de4eda3cd16159ac2d2a4d72ebeacd1bd8e
Steven Rémot <steven.remot@gmail.com>
2014-12-30 11:14:02 +0100

    Disable GUI elements even out of window system
    This prevents these elements to be enabled when emacs starts as a daemon

--- init/ergo.el
+++ init/ergo.el
@@ -9,10 +9,11 @@
 (global-set-key (kbd "C-s-s") 'speedbar)
 
 (require 'uniquify)
-(when window-system
-  (tool-bar-mode -1)
-  (menu-bar-mode -1)
-  (scroll-bar-mode -1))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
 (ido-mode 1)
 
 (setq-default indent-tabs-mode nil)
