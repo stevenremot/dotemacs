69d30a964db12abb64073d17e97c392871d7a022
Steven Rémot <steven.remot@gmail.com>
2015-01-31 15:44:10 +0100

    Rewrite init module loading code with Org mode

--- init.el
+++ init.el
@@ -1,41 +1,4 @@
-;;; init.el --- Where all the magic begins
-;;
-
-;;; Commentary:
-;;
-;;; Code:
-
-(defconst my-confdir (file-name-directory (or buffer-file-name load-file-name)))
-(defconst my-init-dir (concat my-confdir "init/"))
-(defconst my-init-modules '("custom"
-                            "package"
-                            "theme"
-                            "ergo"
-                            "helm"
-                            "cedet"
-                            "jabber"
-                            "gtags"
-                            "flycheck"
-                            "company"
-                            "project"
-                            "eldoc"
-                            ;; "typescript"
-                            "web"
-                            "clojure"
-                            "lisp"
-                            "tlsd"
-                            "haskell"
-                            ;; "mu4e"
-                            "redmine"
-                            "local"
-                            ))
-
-(defun my-init-modules (modules)
-  "Launch MODULES' initialization."
-  (dolist (module modules)
-    (load-file (concat my-init-dir module ".el"))))
-
-(my-init-modules my-init-modules)
-
-(provide 'init)
-;;; init.el ends here
+(require 'org)
+(org-babel-load-file (concat
+                      (file-name-directory (or buffer-file-name load-file-name))
+                      "load-modules.org") t)
