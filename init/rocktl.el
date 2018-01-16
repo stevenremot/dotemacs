6e016cec5836fdd6e8d982c4e5499331da8e2c8f
Steven Rémot <steven.remot@gmail.com>
2018-01-16 14:09:22 +0100

    Add rocktl

---
+++ init/rocktl.el
@@ -0,0 +1,13 @@
+;;; rocktl.el --- Initialize rocktl
+
+;;; Commentary:
+;; 
+;;; Code:
+(require 'use-package)
+
+(use-package rocktl
+  :bind (("C-c & &" . rocktl-run-task)
+	 ("C-c & s" . rocktl-status))
+  :quelpa (rocktl :fetcher github :repo "stevenremot/emacs-rocktl"))
+
+;;; rocktl.el ends here
