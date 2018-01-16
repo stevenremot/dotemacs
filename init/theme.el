480178394b7d77647eb7474e2c752d222393d112
Steven Rémot <steven.remot@gmail.com>
2018-01-16 13:34:58 +0100

    Tweak default font params a bit
    Change font family to source code pro, and make the font size a bit bigger

--- init/theme.el
+++ init/theme.el
@@ -11,13 +11,13 @@
   (tool-bar-mode 0)
   (scroll-bar-mode 0)
   (menu-bar-mode 0)
-  (moe-dark))
+  (moe-dark)
+  (set-face-attribute 'default nil :height 140 :family "Source code pro"))
 
 
 
 (use-package moe-theme
   :ensure
   :init (init/moe-theme))
-
 
 ;;; theme.el ends here
