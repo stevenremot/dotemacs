c3bdca06b0d25edbde32e3f525c12620a41c1dea
Steven Rémot <steven.remot@gmail.com>
2018-01-19 13:51:23 +0100

    Improve flow support
    Bind M-. to xref-find-definitions
    DO a first attempt to run eslint before lsp for a better feedback.

--- init/flow.el
+++ init/flow.el
@@ -1,7 +1,7 @@
 ;;; flow.el --- Setup tools for working with flow typings
 
 ;;; Commentary:
-;; 
+;;
 ;;; Code:
 (require 'use-package)
 (require 'quelpa-use-package)
@@ -10,14 +10,21 @@
   "Setup lsp-javascript-flow package."
   (with-eval-after-load 'lsp-flycheck
     (lsp-flycheck-add-mode 'js-mode)
-    (flycheck-add-next-checker 'lsp 'javascript-eslint)))
+    (flycheck-add-next-checker 'javascript-eslint 'lsp)))
+
+(defun init/javascript-flow-configure ()
+  "Configure JS buffers."
+  (lsp-javascript-flow-enable)
+  (setq flycheck-checker 'javascript-eslint)
+  (local-set-key (kbd "M-.") #'xref-find-definitions))
 
 
 
 (use-package lsp-javascript-flow
   :ensure
   :quelpa (lsp-javascript-flow :fetcher github :repo "stevenremot/emacs-lsp-javascript-flow")
-  :hook (js-mode . lsp-javascript-flow-enable)
+  :hook (js-mode . init/javascript-flow-configure)
+  :commands (lsp-javascript-flow-enable)
   :init (init/setup-lsp-javascript-flow))
 
 ;;; flow.el ends here
