899db3555d6bfef99a9d4a125340a4e664ac2459
Steven Rémot <steven.remot@gmail.com>
2018-02-03 13:31:58 +0100

    Add json support

--- init/javascript.el
+++ init/javascript.el
@@ -1,12 +1,15 @@
 ;;; javascript.el --- Setup javascript tools
 
 ;;; Commentary:
-;; 
+;;
 ;;; Code:
 (require 'use-package)
 
 (use-package add-node-modules-path
   :ensure
   :hook ((js-mode . add-node-modules-path)))
+
+(use-package json-mode
+  :ensure)
 
 ;;; javascript.el ends here
