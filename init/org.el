81bf44d9b1239f4fb46e31f75e91864c43bb6eee
Steven Rémot <steven.remot@gmail.com>
2018-02-14 13:34:43 +0100

    Improve Org mode configuration
    Add a shortcut for org-todo-list, and fontify src blocks
    automatically.

--- init/org.el
+++ init/org.el
@@ -5,6 +5,11 @@
 
 ;;; Code:
 
+(use-package org
+  :bind (("C-c o t" . org-todo-list))
+  :init
+  (setq org-src-fontify-natively t))
+
 (use-package org-agenda
   :bind (("C-c o a" . org-agenda-list)))
 
