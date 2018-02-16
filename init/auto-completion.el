7abff87483067620e4b79fa735984b442ca92c7b
Steven Rémot <steven.remot@gmail.com>
2018-02-16 13:43:43 +0100

    Bind C-M-i to company

--- init/auto-completion.el
+++ init/auto-completion.el
@@ -1,12 +1,13 @@
 ;;; auto-completion.el --- Setup code completion
 
 ;;; Commentary:
-;; 
+;;
 ;;; Code:
 (require 'use-package)
 
 (use-package company
   :ensure
+  :bind (("C-M-i" . company-complete))
   :init (global-company-mode))
 
 ;;; auto-completion.el ends here
