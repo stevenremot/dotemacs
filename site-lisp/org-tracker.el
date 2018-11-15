5ee626fb257d6cae46f7bee32fcb50f0c8027261
Steven Rémot <steven.remot@gmail.com>
2018-11-15 13:26:27 +0100

    Rename org-open-ref to org-tracker

--- site-lisp/org-tracker.el
+++ site-lisp/org-tracker.el
@@ -1,4 +1,4 @@
-;;; org-open-refs.el --- Opens references to your issues in org mode -*- lexical-binding: t -*-
+;;; org-trackers.el --- Opens references to your issues in org mode -*- lexical-binding: t -*-
 
 ;;; Commentary:
 ;; This package allows you to write reference to issues in your org
@@ -8,11 +8,11 @@
 (require 'org-clock)
 (require 'org-agenda)
 
-(defun org-open-ref-get-ref-at-point ()
+(defun org-tracker-get-ref-at-point ()
   "Return the reference currently at point."
   (number-to-string (thing-at-point 'number)))
 
-(defun org-open-ref--redmine-open (pom reference)
+(defun org-tracker-open-issue:redmine (pom reference)
   "Open a reference in redmine.
 
 POM is a point in the org document.
@@ -22,7 +22,7 @@ REFERENCE is the reference."
       (error "The property \"redmine-repo\” must be set"))
     (browse-url (format "%s/issues/%s" url reference))))
 
-(defun org-open-ref--gitlab-open (pom reference)
+(defun org-tracker-open-issue:gitlab (pom reference)
   "Open a reference in gitlab.
 
 POM is a point in the org document.
@@ -32,48 +32,48 @@ REFERENCE is the reference."
       (error "The property \"gitlab-repo\” must be set"))
     (browse-url (format "%s/issues/%s" url reference))))
 
-(defun org-open-ref-get-system (pom)
+(defun org-tracker-get-system (pom)
   "Return the ref system to use at POM."
-  (org-entry-get pom "ref-system" t))
+  (org-entry-get pom "tracker" t))
 
-(defun org-open-ref (pom reference)
+(defun org-tracker-open-issue (pom reference)
   "Open the reference currently at POM.
 
 POM is a point in the org document.
 REFERENCE is the reference."
-  (pcase (org-open-ref-get-system pom)
-    ("redmine" (org-open-ref--redmine-open pom reference))
-    ("gitlab" (org-open-ref--gitlab-open pom reference))
-    (_ (error "Could not recognize %S ref-system"(org-open-ref-get-system pom)))))
+  (pcase (org-tracker-get-system pom)
+    ("redmine" (org-tracker-open-issue:redmine pom reference))
+    ("gitlab" (org-tracker-open-issue:gitlab pom reference))
+    (_ (error "Could not recognize %S ref-system"(org-tracker-get-system pom)))))
 
 ;;;###autoload
-(defun org-open-ref-at-point ()
+(defun org-tracker-open-issue-at-point ()
   "Open the reference at point."
   (interactive)
   (save-window-excursion
-    (let ((reference (org-open-ref-get-ref-at-point))
+    (let ((reference (org-tracker-get-ref-at-point))
 	  (marker (org-agenda-get-any-marker)))
       (when (eq major-mode 'org-agenda-mode)
 	(switch-to-buffer (marker-buffer marker))
 	(goto-char (marker-position marker)))
-      (org-open-ref (point) reference))))
+      (org-tracker-open-issue (point) reference))))
 
-(defun org-open-ref--get-current-issue ()
+(defun org-tracker--get-current-issue ()
   "Return the reference in the org item currently clocked in."
   (when (and org-clock-current-task
 	   (numberp (string-match (rx (1+ num)) org-clock-current-task)))
       (string-to-number (match-string 0 org-clock-current-task))))
 
 ;;;###autoload
-(defun org-open-ref-for-current-issue ()
+(defun org-tracker-open-current-issue ()
   "Open the reference of the org issue currently clocked in."
   (interactive)
-  (let ((reference (org-open-ref--get-current-issue)))
+  (let ((reference (org-tracker--get-current-issue)))
     (unless reference
       (error "No item currently clocked in"))
-    (org-open-ref org-clock-marker reference)))
+    (org-tracker-open-issue org-clock-marker reference)))
 
 
-(provide 'org-open-ref)
+(provide 'org-tracker)
 
-;;; org-open-ref.el ends here
+;;; org-tracker.el ends here
