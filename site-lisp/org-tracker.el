9a85d184793e4dd9923ebed203cbc9f9b4ad490c
Steven Rémot <steven.remot@gmail.com>
2019-01-27 12:01:23 +0100

    Track clock entries with comments on redmine

--- site-lisp/org-tracker.el
+++ site-lisp/org-tracker.el
@@ -33,8 +33,17 @@ Return nil when no tracker could be created."
 
 (defun org-tracker-get-ref-in-string (string)
   "Return first issue reference in STRING."
-  (when (numberp (string-match (rx (1+ num)) string))
-      (string-to-number (match-string 0 string))))
+  (when (numberp (string-match (rx "#" (group (1+ alnum)) word-boundary) string))
+    (match-string 1 string)))
+
+(defun org-tracker-get-issue-for-point ()
+  "Return the issue reference in POM context.
+
+Go up the item hierarchy until some item contains an issue or
+root is reached."
+      (->> (org-get-outline-path t)
+	 (seq-map #'org-tracker-get-ref-in-string)
+	 (seq-find #'stringp)))
 
 (cl-defgeneric org-tracker-open-issue (_tracker _reference)
   "Open the issue at point.
@@ -70,15 +79,70 @@ REFERENCE is the issue reference."
 
 ;; Time tracking
 
-(cl-defmethod org-tracker-track-time (_tracker _issue-id _date _hours _callback)
+(cl-defmethod org-tracker-track-time (_tracker _issue-id _date _hours _comment _callback)
   "Track time for an issue.
 
 TRACKER is the current tracker instance.
 ISSUE-ID is the reference of the issue to track in.
-DATE is the date of the day to spend time in (YYYY-MM-DD).
+DATE is the date of the day to spend time in emacs timestamp format.
 HOURS is the number of hours to spend.
+COMMENT is an optional comment string associated to the log.
 CALLABCK is a function called when the tracking is completed."
   (error "Method org-tracker-track-time not implemented for this tracker"))
+
+(defun org-tracker-get-timestamp-start-date (timestamp)
+  "Return the start date of an org TIMESTAMP element."
+  (encode-time 0
+	       (org-element-property :minute-start timestamp)
+	       (org-element-property :hour-start timestamp)
+	       (org-element-property :day-start timestamp)
+	       (org-element-property :month-start timestamp)
+	       (org-element-property :year-start timestamp)))
+
+(defun org-tracker-get-timestamp-end-date (timestamp)
+  "Return the end date of an org TIMESTAMP element."
+  (encode-time 0
+	       (org-element-property :minute-end timestamp)
+	       (org-element-property :hour-end timestamp)
+	       (org-element-property :day-end timestamp)
+	       (org-element-property :month-end timestamp)
+	       (org-element-property :year-end timestamp)))
+
+(defun org-tracker-get-tracking-comment-for-point ()
+  "Return the comment that should be associated to the current tracking."
+  (let ((outline-path (org-get-outline-path t))
+	(comment-list '())
+	(passed-issue-id nil))
+    (dolist (item outline-path)
+      (if passed-issue-id
+	  (setq comment-list (push item comment-list))
+	(when (stringp (org-tracker-get-ref-in-string item))
+	  (setq passed-issue-id t))))
+    (mapconcat #'identity (reverse comment-list) " - ")))
+
+(defun org-tracker-track-time-at-point (pom)
+  "Track time using the clock entry at POM."
+  (interactive "d")
+  (let ((element (org-element-at-point))
+	(tracker (org-tracker-get-current-tracker pom))
+	timestamp start-date end-date duration)
+    (unless (eq (org-element-type element) 'clock)
+      (error "Point is not on a clock entry"))
+
+    (setq timestamp (org-element-property :value element)
+	  start-date (org-tracker-get-timestamp-start-date timestamp)
+	  end-date (org-tracker-get-timestamp-end-date timestamp)
+	  duration (/ (float-time (time-subtract end-date start-date)) 3600))
+
+    (org-tracker-track-time tracker
+			    (org-tracker-get-issue-for-point)
+			    start-date
+			    duration
+			    (org-tracker-get-tracking-comment-for-point)
+			    (lambda (&optional err)
+			      (if err
+				  (error err)
+				(message "Timestamp tracked"))))))
 
 ;; Dummy tracker - for testing purposes
 
@@ -92,8 +156,12 @@ CALLABCK is a function called when the tracking is completed."
 (cl-defmethod org-tracker-open-issue ((_tracker org-tracker-dummy) reference)
   (message "Browsing issue %s" reference))
 
-(cl-defmethod org-tracker-track-time ((_tracker org-tracker-dummy) issue-id date hours callback)
-  (message "Tracking %f hours at %s for %s" hours date issue-id)
+(cl-defmethod org-tracker-track-time ((_tracker org-tracker-dummy) issue-id date hours comment callback)
+  (message "Tracking %f hours at %s for %s with comment %s"
+	   hours
+	   (format-time-string "%F" date)
+	   issue-id
+	   comment)
   (funcall callback))
 
 ;; Redmine
@@ -110,17 +178,26 @@ CALLABCK is a function called when the tracking is completed."
   (let ((url (oref tracker repo)))
     (browse-url (format "%s/issues/%s" url reference))))
 
-;; (defun org-tracker-track-time ((tracker org-tracker-redmine) issue-id date hours callback)
-;;   "Track time in a redmine issue.
+(cl-defmethod org-tracker-track-time ((tracker org-tracker-redmine) issue-id date hours comment callback)
+  "Track time in a redmine issue.
 
-;; PROPERTIES is a property list for the configuration.
-;; ISSUE-ID is the id of the issue.
-;; DATE is the date the time was spent.
-;; HOURS is the number of spent hours.
-;; CALLBACK is the function called when the time is tracked."
-;;   (let ((elmine/host (plist-get properties :redmine-repo))
-;; 	(elmine/api-key (plist-get properties :redmine-api-key)))
-;;     (funcall callback (elmine/create-time-entry :issue_id issue-id :date date :hours hours))))
+PROPERTIES is a property list for the configuration.
+ISSUE-ID is the id of the issue.
+DATE is the date the time was spent.
+HOURS is the number of spent hours.
+CALLBACK is the function called when the time is tracked."
+  (let* ((elmine/host (oref tracker repo))
+	 (elmine/api-key (plist-get (auth-source-search :host elmine/host
+							:max 1
+							:require '(:secret))
+				    :secret)))
+    (elmine/create-time-entry
+     :issue_id issue-id
+     :date (format-time-string "%F" date)
+     :hours hours
+     :comments comment)
+
+    (funcall callback nil)))
 
 ;; Gitlab
 (defclass org-tracker-gitlab ()
