7d4142ac7550ed875881c754dca08c125375f926
Steven Rémot <steven.remot@gmail.com>
2020-06-18 11:37:32 +0200

    Fix error when isntallig from scratch
    * Do not try to fetch rocktl from elpa
    * Do not fetch vterm at compile time and at startup, as it requires
      cmake for first install

--- init/rocktl.el
+++ init/rocktl.el
@@ -1,11 +1,12 @@
 ;;; rocktl.el --- Initialize rocktl
 
 ;;; Commentary:
-;; 
+;;
 ;;; Code:
 (require 'use-package)
 
 (use-package rocktl
+  :ensure nil
   :bind (("C-c & &" . rocktl-run-task)
 	 ("C-c & s" . rocktl-status))
   :quelpa (rocktl :fetcher github :repo "stevenremot/emacs-rocktl"))
