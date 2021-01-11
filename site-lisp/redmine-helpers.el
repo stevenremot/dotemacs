7892b58795d8c22c24a08d1e71f977d38705cae0
Steven Rémot <steven.remot@gmail.com>
2021-01-11 16:13:59 +0100

    Improve redmine helpers
    Do not rely on org capture that requires per-project setup

--- site-lisp/redmine-helpers.el
+++ site-lisp/redmine-helpers.el
@@ -6,17 +6,52 @@
 (require 'org-capture)
 (require 'redmine-api)
 
+(defconst my-redmine-project-regexp (rx line-start "*" (+ whitespace) (group (* alnum)) line-end)
+  "Regexp for matching a project section.")
+
+(defun my-redmine-list-projects ()
+  "List all the available projects in the buffer."
+  (save-excursion
+    (goto-char (point-min))
+
+    (let ((projects '()))
+      (while (re-search-forward my-redmine-project-regexp nil :no-error)
+	(setq projects (push (match-string 1) projects)))
+      projects)))
+
+(defun my-redmine-list-task-projects ()
+  "List all available projects in tasks buffer."
+  (save-excursion
+    (bookmark-jump "Tasks")
+    (my-redmine-list-projects)))
+
+(defun my-redmine-goto-task-project (project)
+  "Go to a specific PROJECT."
+  (bookmark-jump "Tasks")
+  (goto-char (point-min))
+  (re-search-forward (rx-to-string `(group line-start "*" (+ whitespace) ,project)))
+
+  (next-line)
+
+  (when (org-at-property-block-p)
+    (goto-char (cdr (org-get-property-block))))
+
+  (end-of-line))
+
+
 ;;;###autoload
-(defun my-redmine-capture-issue (issue-id template)
-  "Start a capturing a redmine issue with id ISSUE-ID and set it using capture TEMPLATE."
+(defun my-redmine-insert-issue (issue-id project)
+  "Insert a redmine issue with id ISSUE-ID as a level 2 org title under PROJECT."
   (interactive (list (read-string "Issue: ")
-		     (car (org-capture-select-template))))
+		     (completing-read "Project: " (my-redmine-list-task-projects))))
   (redmine-api-get-issue
    issue-id
    :success #'(lambda (issue)
 		(let-alist issue
-		  (org-capture nil template)
-		  (insert (format "%s - %s" issue-id .subject))))
+		  (my-redmine-goto-task-project project)
+		  (newline)
+		  (org-insert-todo-subheading (point))
+		  (insert (format "#%s - %s" issue-id .subject))))
    :error #'(lambda (&rest args)
 	      (error "%S" args))))
 
