c198ee17de017680018526fe9c548e4df73d1a86
Steven Remot <steven.remot@stevenremot.local>
2022-01-28 10:32:30 +0100

    Add sonar command

---
+++ site-lisp/sonar.el
@@ -0,0 +1,30 @@
+;;; sonar.el --- Sonar helpers
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(defcustom sonar-url ""
+  "Sonar base URL.")
+
+;;;###autoload
+(put 'sonar-url 'safe-local-variable 'stringp)
+
+(defcustom sonar-project-id ""
+  "Sonar project ID.")
+
+;;;###autoload
+(put 'sonar-project-id 'safe-local-variable 'stringp)
+
+;;;###autoload
+(defun sonar-visit-file-page ()
+  "Open the sonar page with the file issues in the web browser."
+  (interactive)
+  (let ((file-name (buffer-file-name (current-buffer)))
+	(project-directory (projectile-project-root)))
+    (browse-url (format "%s/code?id=%s&selected=%s:%s" sonar-url sonar-project-id sonar-project-id (file-relative-name file-name project-directory)))))
+
+(provide 'sonar)
+
+;;; sonar.el ends here
