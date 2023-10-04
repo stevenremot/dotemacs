3b9d1b58446723adc545e65c1adbab424f934eef
StevenRemot <steven.remot@goshaba.com>
2023-10-04 09:13:28 +0200

    test-watcher: Add the beginning of a junit visualizer

--- site-lisp/test-watcher.el
+++ site-lisp/test-watcher.el
@@ -4,7 +4,7 @@
 ;;
 
 (require 'project)
-(require 'vterm)
+(require 'cl-lib)
 
 ;;; Code:
 
@@ -25,6 +25,7 @@
 (defun tw-run ()
   "Run the test command in vterm for the current project."
   (interactive)
+  (require 'vterm)
   (let ((command (completing-read "Test command: " tw-shell-commands nil nil))
 	(default-directory (project-root (project-current))))
     (vterm (format "*%s test*" (thread-first default-directory
@@ -39,6 +40,28 @@
   (let* ((path-to-use (completing-read "Coverage path:" tw-coverage-paths nil nil))
 	 (path (expand-file-name path-to-use (project-root (project-current)))))
     (xwidget-webkit-browse-url (format "file://%s" path))))
+
+;;;###autoload
+(defun tw-junit-org-report-for-buffer ()
+  "Create an org report of the currently visited junit.xml file."
+  (interactive)
+  (let ((junit-data (libxml-parse-xml-region (point-min) (point-max)))
+	(report-buffer (get-buffer-create "*junit-org*")))
+    (with-current-buffer report-buffer
+      (erase-buffer)
+
+      (let-alist (nth 1 junit-data)
+	(insert (format "#+TITLE: %s\n\n" .name)))
+
+      (cl-loop for suite-pair in (cddr junit-data)
+	       for suite-attrs = (nth 1 suite-pair)
+	       for suite-elements = (cddr suite-pair)
+	       do
+	       (let-alist suite-attrs
+		 (insert (format "* %s\n\n" .name))))
+
+      (org-mode)
+      (display-buffer (current-buffer)))))
 
 (provide 'test-watcher)
 
